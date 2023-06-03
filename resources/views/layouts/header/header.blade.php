f1398440770308f1d22fdf52f43025212e3c342f
Francisco Costa <francisco100eg@gmail.com>
2023-06-03 19:29:35 -0300

    :Feat: Pet card

---
+++ resources/views/layouts/header/header.blade.php
@@ -0,0 +1,40 @@
+
+<header class="Header">
+    <nav class="Header__container">
+        <div class="Header__logo">
+            <a href="">
+                <img src="{{ asset('logo.png') }}" alt="Logo AdoteAumigos">
+            </a>
+        </div>
+        <div class="Header__links">
+            <ul class="Header_links-list">
+                <li class="Header__link">
+                    <div class="link-point"></div>
+                    <a href=""  class="link">Home</a>
+                </li>
+                <li class="Header__link">
+                    <div class="link-point"></div>
+                    <a href=""  class="link">Sobre nós</a>
+                </li>
+                <li class="Header__link">
+                    <div class="link-point"></div>
+                    <a href="" class="link">Contato</a>
+                </li>
+                <li>
+                    <div class="link-point"></div>
+                    <a href=""  class="link">Adotar</a>
+                </li>
+                <li>
+                    <div class="link-point"></div>
+                    <a href=""  class="link">Doar</a>
+                </li>
+                <li>
+                    <div class="link-point"></div>
+                    <a href=""  class="link">Sair</a>
+                </li>
+                </ul>
+        </div>
+        </div>
+
+    </nav>
+</header>
